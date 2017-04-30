ad51ffd7e53a9d39e08517e3effce960f94335d0
Nikolaj Ivancic <nik@congral.com>
2017-04-30 15:22:26 -0400

    no comment

--- skeleton-esnext/src/app.js
+++ skeleton-esnext/src/app.js
@@ -1,7 +1,30 @@
 import {inject} from 'aurelia-framework';
 import { RedirectToRoute } from 'aurelia-router';
+import {Auth0Service} from './auth0-service';
+
+
+@inject(Auth0Service)
 
 export class App {
+  constructor(auth0Service) {
+    let _lock = auth0Service.getLock();
+
+    _lock.on("authenticated", (authResult) => {
+      _lock.getProfile(authResult.idToken, (error, profile) => {
+        if (error) {
+          console.log("getProfile failed.")
+          return;
+        }
+
+        let _profile = JSON.stringify(profile);
+        let _id_token = authResult.idToken;
+
+        _lock.hide();
+      });
+    });
+
+
+  }
 
   configureRouter(config, router) {
 
--- skeleton-esnext/src/login.js
+++ skeleton-esnext/src/login.js
@@ -1,26 +1,11 @@
-import {inject} from 'aurelia-framework';  
+import {inject} from 'aurelia-framework';
 import {Auth0Service} from './auth0-service';
 
 @inject(Auth0Service)
 export class Login {
 
-	 constructor(auth0Service) {
-   	this.lock = auth0Service.getLock();
-	  this.lock.show();
-
-    //var self = this;
-    this.lock.on("authenticated", (authResult) => {
-      this.lock.getProfile(authResult.idToken, (error, profile) => {
-        if (error) {
-          // Handle error
-          return;
-        }
-
-        localStorage.setItem('id_token', authResult.idToken);
-        localStorage.setItem('profile', JSON.stringify(profile));
-        this.isAuthenticated = true;
-        this.lock.hide();
-      });
-    });
+  constructor(auth0Service) {
+    let _lock = auth0Service.getLock();
+    _lock.show();
   }
 }
